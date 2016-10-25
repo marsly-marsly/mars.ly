6dc8d120c2f241508e6be37c705717c17600d259
marsly-marsly <mars.ly.website@gmail.com>
2016-10-25 05:10:55 -0400

    Add files via upload

---
+++ js/variables.js
@@ -0,0 +1,104 @@
+// the articles number
+Art_Number	= "17";
+
+// article No 01
+type_1  = "op-ed";
+Name_1 	= "interview-with-musk";
+Image_1 = "img_31.png";
+Body_1	= "Recent Interview with Elon Musl about Mars and Byond";
+
+// article No 02
+type_2  = "blog";
+Name_2 	= "blog";
+Image_2 = "img_32.png";
+Body_2	= "Site of First Mars Hotel";
+
+// article No 03
+type_3  = "blog";
+Name_3 	= "blog-mars-colony-one";
+Image_3 = "img_38.png";
+Body_3	= "NASA Plans to Build First Colony on Mars by 2033.";
+
+// article No 04
+type_4  = "blog";
+Name_4 	= "blog-mars-life";
+Image_4 = "img_39.png";
+Body_4	= "Scientists Have Found Sings of Life on Mars in an Unexpected Place";
+
+// article No 05
+type_5  = "blog";
+Name_5 	= "blog-mars-mountains";
+Image_5 = "img_33.png";
+Body_5	= "Mars' Biggest Mountain May be the Best Place to Find Minerals";
+
+// article No 06
+type_6  = "blog";
+Name_6 	= "blog-rover";
+Image_6 = "img_40.png";
+Body_6	= "Future Mars Roves will Have More Capability Thanks to New Metal";
+
+// article No 07
+type_7  = "journal";
+Name_7 	= "mars-first-travel";
+Image_7 = "img_37.png";
+Body_7	= "First Vistors to Mars Reporting of an Incredible Journey";
+
+// article No 08
+type_8  = "op-ed";
+Name_8 	= "not-op-ed";
+Image_8 = "img_34.png";
+Body_8	= "Lorem ipsum dolor sit amet, consectetur adipisicing elit.";
+
+// article No 09
+type_9  = "op-ed";
+Name_9 	= "ramsi-mars-op-ed";
+Image_9 = "img_39.png";
+Body_9	= "Lorem ipsum dolor sit amet, consectetur adipisicing elit.";
+
+// article No 10
+type_10  = "news";
+Name_10 = "news-second-mission";
+Image_10 = "img_35.png";
+Body_10	= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet";
+
+// article No 11
+type_11  = "news";
+Name_11 = "travel-to-mars";
+Image_11 = "img_42.png";
+Body_11	= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Article 6";
+
+// article No 12
+type_12  = "journal";
+Name_12 	= "mars-second-travel";
+Image_12 = "img_40.png";
+Body_12	= "Lorem ipsum dolor sit amet, consectetur adipisicing elit.";
+
+// article No 13
+type_13  = "journal";
+Name_13 = "recent-mars-news";
+Image_13 = "img_43.png";
+Body_13	= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. ";
+
+// article No 14
+type_14  = "blog";
+Name_14 = "blog_mars_colony";
+Image_14 = "img_46.png";
+Body_14	= "NASA Plans to Build First Colony on Mars by 2033.";
+
+// article No 15
+type_15  = "news";
+Name_15 = "news-first-mission";
+Image_15 = "img_35.png";
+Body_15	= "Exclusive Report from First Mars Mission";
+
+// article No 16
+type_16  = "news";
+Name_16 = "mars-oxygen";
+Image_16 = "img_45.png";
+Body_16	= "Mars Oxygen Level are Rising Due to Volcano Activities.";
+
+// article No 17
+type_17  = "journal";
+Name_17 = "recent-mission";
+Image_17 = "img_47.png";
+Body_17	= "Recent Mission to Mars Ended in Disaster";
